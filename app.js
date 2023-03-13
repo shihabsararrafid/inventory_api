3b8c4c4de4230ea1de11d1153f5ccd4ad1b7e3a5
Shihab Sarar Rafid <shrafid.532@gmail.com>
2023-03-13 12:00:46 +0600

    change the coming data value by creating instance of model

--- app.js
+++ app.js
@@ -7,75 +7,113 @@ app.use(express.json());
 app.use(cors());
 
 // schema design
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide a name for this product"],
-    trim: true,
-    unique: [true, "Product name must be unique"],
-    minLength: [3, "Name Must be at least 3 characters"],
-    maxLength: [100, "Name is too large"],
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: [0, "Price Can't be negative"],
-  },
-  unit: {
-    type: String,
-    required: true,
-    enum: {
-      value: ["kg", "litre", "pcs"],
-      message: "unit value can't be {VALUE}, must be kg/litre/pcs",
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name for this product"],
+      trim: true,
+      unique: [true, "Product name must be unique"],
+      minLength: [3, "Name Must be at least 3 characters"],
+      maxLength: [100, "Name is too large"],
     },
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    min: [0, "Quantity can't be negative"],
-    validate: {
-      validator: (value) => {
-        const isInteger = Number.isInteger(value);
-        if (isInteger) return true;
-        else return false;
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, "Price Can't be negative"],
+    },
+    unit: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["kg", "litre", "pcs"],
+        message: "unit value can't be {VALUE}, must be kg/litre/pcs",
       },
-      message: "Quantity must be an integer",
     },
-  },
-  status: {
-    type: String,
-    required: true,
-    enum: {
-      value: ["In-stock", "out-of-stock", "discontinued"],
-      message: "status can't be {VALUE}",
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "Quantity can't be negative"],
+      validate: {
+        validator: (value) => {
+          const isInteger = Number.isInteger(value);
+          if (isInteger) return true;
+          else return false;
+        },
+        message: "Quantity must be an integer",
+      },
     },
-  },
-  supplier: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Supplier",
-  },
-  categories:[{
-   name:{
-  type:String,
-  required:true
-  },
-_id:mongoose.Schema.Types.ObjectId ,
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["In-stock", "out-of-stock", "discontinued"],
+        message: "status can't be {VALUE}",
+      },
+    },
+    // supplier: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "Supplier",
+    // },
 
-
-  }]
-  // createdAt: {
-  //   type: Date,
-  //   default: Date.now,
-  // },
-  timestamps: true,
-});
+    // categories: [
+    //   {
+    //     name: {
+    //       type: String,
+    //       required: true,
+    //     },
+    //     _id: mongoose.Schema.Types.ObjectId,
+    //   },
+    // ],
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },{
+  },
+  {
+    timestamps: true,
+  }
+);
+// SCHEMA -> MODEL -> Query
+// creating models
+// here mongoose model function takes two parameters one is the name of the model and another is the schema
+const Product = mongoose.model("Product", productSchema);
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
+});
+//creating post request
+app.post("/api/v1/product", async (req, res, next) => {
+  // for saving there is two options in mongoose
+  /***
+   * One is to use save by creating an instance
+   * Another is to create without creating an instance
+   */
+  //console.log({ Product });
+  try {
+    const product = new Product(req.body);
+    if (product.quantity === 0) {
+      product.status = "out-of-stock";
+    }
+    const result = await product.save();
+    console.log(result);
+    // console.log(req.body);
+    res.status(200).json({
+      status: "Success",
+      message: "Data saved successfully",
+      result: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "fail to save the data",
+      error: error.message,
+    });
+  }
 });
 
 module.exports = app;
